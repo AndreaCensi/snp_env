1094c4fbd705187d2ade92dfbafe95f3ef52233b
Andrea Censi <andrea@cds.caltech.edu>
2010-10-19 15:44:31 -0700

    misc

--- data/tammero.sh
+++ data/tammero.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
+set -x
 
-a
+dir=landing/detection/kf-oct17
+
+sac_tammero_analysis --saccades $dir/saccades-noposts.h5   --output tammero_analysis/noposts/
+sac_tammero_analysis --saccades $dir/saccades-posts.h5     --output tammero_analysis/posts/
