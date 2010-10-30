d9167215d3af9363510deb2fd6730d2be69b5aef
Andrea Censi <andrea@cds.caltech.edu>
2010-10-30 15:00:43 -0700

    updated script

--- data/render_all.sh
+++ data/render_all.sh
@@ -19,3 +19,12 @@ flydra_video_image --db $DB --image luminance   --filter plot_luminance  $ids
 flydra_video_image --db $DB --image contrast    --filter plot_contrast $ids
 flydra_video_image --db $DB --image luminance_w --filter plot_luminance $ids
 flydra_video_image --db $DB --image contrast_w  --filter plot_contrast $ids
+
+# render big videos
+flydra_run_pg_model --db $DB --model flydra_display_luminance --needs rows,luminance $ids
+flydra_run_pg_model --db $DB --model flydra_display_contrast --needs rows,luminance,contrast,contrast_w $ids
+
+
+# analysis
+
+mamarama_first_order --db $DB
