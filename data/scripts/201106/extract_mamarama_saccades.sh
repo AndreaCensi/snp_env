0b121b3b261cb6d761d4787f6626acb98cf9e8c3
Andrea Censi <andrea@cds.caltech.edu>
2011-07-23 16:58:44 -0700

    scripts working

--- data/scripts/201106/extract_mamarama_saccades.sh
+++ data/scripts/201106/extract_mamarama_saccades.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 set -e
-#set -x
+set -x
 
 INPUT_DIR=${SNPENV_DATA}/source/landing
 OUT_DIR=${SNPENV_DATA}/reports/201106/
-TMP_DIR=${SNPENV_DATA}/processed/201106/geo_sac_detect
 DB_DIR=${SNPENV_DATA}/processed/201106/flydra_db
+DATA_DIR=${SNPENV_DATA}/processed/201106/compmake
 
-# Filter data, put in DB
-echo flydra_render_filter --db ${DB_DIR} --smooth ${INPUT_DIR}
+# # Filter data, put in DB
+flydra_render_filter --db ${DB_DIR} ${INPUT_DIR} --smooth 
+flydra_render_filter --db ${DB_DIR} ${INPUT_DIR}
+
+# # Extract saccades
+geo_sac_detect_flydra --db ${DB_DIR} --version kf     #--nocache
+geo_sac_detect_flydra --db ${DB_DIR} --version smooth #--nocache
+
+# Analyze saccade
+args="--db ${DB_DIR} --datadir ${DATA_DIR}/sac_density_report --outdir ${OUT_DIR}/sac_density_report"
+sac_density_report $args --version smooth --group nopost  --compmake "make"
+sac_density_report $args --version smooth --group posts   --compmake "make"
+# sac_density_report $args --version kf  --group nopost  --compmake "make"
+# sac_density_report $args --version kf  --group posts   --compmake "make"
+ 
+# sac_density_report $args --version kf  --group nopost --compmake  "remake *joint* *report*" 
+#--compmake "remake *joint* report_models_choice write*"
+# sac_density_report $args --version kf  --group posts  --compmake make # $cm 
 
 #sac_density_report --db ${DB_DIR} --outdir ${OUT_DIR}/density --group nopost
-sac_density_report --compmake_command "remake *joint* *report*" --db ${DB_DIR} --outdir ${OUT_DIR}/density --group nopost
-echo sac_density_report --db ${DB_DIR} --outdir ${OUT_DIR}/density --group posts
-
-
-# Extract saccades
-echo geo_sac_detect --output ${TMP_DIR} ${INPUT_DIR}
-echo geo_sac_to_flydradb --db ${DB_DIR} ${TMP_DIR}
-
-# Analyze saccades
-echo sac_spontaneous_analysis --db ${DB_DIR} --outdir ${OUT_DIR}/spontaneous_analysis 
+# sac_density_report --compmake  "remake *joint* *report*" --db ${DB_DIR} --outdir ${OUT_DIR}/density --group nopost
+# sac_density_report --compmake  "remake *report_models_choice* *write*" --db ${DB_DIR} --outdir ${OUT_DIR}/density --group nopost
+# 
+# # Analyze saccades
+# echo sac_spontaneous_analysis --db ${DB_DIR} --outdir ${OUT_DIR}/spontaneous_analysis 
